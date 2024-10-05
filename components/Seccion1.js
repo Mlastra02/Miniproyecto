323385fe38ffe651f2fd640a6c7f18d5ccf55ec5
Agustin de la Vega <agustin.delavega02@gmail.com>
2024-10-04 23:52:11 -0300

    arreglo componente seccion1

--- components/Seccion1.js
+++ components/Seccion1.js
@@ -1,16 +1,15 @@
 // components/Seccion1.js
-export default function Seccion1() {
+export default function Seccion1({ title, description }) {
   return (
     <section
       id="seccion1"
       className="text-center py-4 mx-4 mt-4 text-soft-dark"
     >
       <h2 className="text-4xl font-extrabold mb-6 text-teal-700 font-sans text-soft-dark">
-        Eventos Deportivos
+        {title}
       </h2>
       <p className="text-xl leading-relaxed mx-auto max-w-prose font-medium italic">
-        Registra tu evento deportivo y participa en las competencias registrada
-        en la base de datos de AMJ Innovation.
+        {description}
       </p>
     </section>
   );
