a997894596fed58e3d756eeb358c76b3f17540ba
Agustin de la Vega <agustin.delavega02@gmail.com>
2024-10-04 22:03:24 -0300

    arreglo de formulario y seccion de info

--- components/Seccion1.js
+++ components/Seccion1.js
@@ -1,13 +1,17 @@
 // components/Seccion1.js
 export default function Seccion1() {
-    return (
-        <section id="seccion1" className="text-center py-4 mx-4 mt-4 text-soft-dark">
-            <h2 className="text-4xl font-extrabold mb-6 text-teal-700 font-sans text-soft-dark">
-                Eventos Deportivos
-            </h2>
-            <p className="text-xl leading-relaxed mx-auto max-w-prose font-medium italic">
-                Inscrite en nuestros eventos deportivos y disfruta de la competencia sana y el compañerismo.
-            </p>
-        </section>
-    );
+  return (
+    <section
+      id="seccion1"
+      className="text-center py-4 mx-4 mt-4 text-soft-dark"
+    >
+      <h2 className="text-4xl font-extrabold mb-6 text-teal-700 font-sans text-soft-dark">
+        Eventos Deportivos
+      </h2>
+      <p className="text-xl leading-relaxed mx-auto max-w-prose font-medium italic">
+        Registra tu evento deportivo y participa en las competencias registrada
+        en la base de datos de AMJ Innovation.
+      </p>
+    </section>
+  );
 }
